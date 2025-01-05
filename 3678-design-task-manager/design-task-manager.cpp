822e6a9a289901f7333da309333b8a277fbb1707
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-01-05 12:56:29 +0500

    Time: 464 ms (100.00%) | Memory: 381.3 MB (80.00%) - LeetSync

--- 3678-design-task-manager/design-task-manager.cpp
+++ 3678-design-task-manager/design-task-manager.cpp
@@ -1,40 +1,47 @@
-using pii = pair<int, int>;
-
 class TaskManager {
-    multiset<array<int, 3>> S;
-    map<int, pii> mp2;
-
 public:
-    TaskManager(vector<vector<int>> &tasks) {
-        for (auto &it: tasks) {
+    // multiset to set the priority, it automatically sorts.
+    multiset<array<int, 3>> S;
+    map<int, pair<int, int>> m;
+    TaskManager(vector<vector<int>>& tasks) {
+        for(auto it: tasks) {
             int x = it[0], y = it[1], z = it[2];
-            mp2[y] = {x, z};
+            m[y] = {x, z};
             S.insert({z, y, x});
         }
     }
-
-    void add(int x, int y, int z) {
-        mp2[y] = {x, z};
-        S.insert({z, y, x});
+    
+    void add(int userId, int taskId, int priority) {
+        m[taskId] = {userId, priority};
+        S.insert({priority, taskId, userId});        
     }
-
-    void edit(int y, int nz) {
-        auto [x, z] = mp2[y];
-        mp2[y] = {x, nz};
-        S.extract({z, y, x});
-        S.insert({nz, y, x});
+    
+    void edit(int taskId, int newPriority) {
+        auto [x, z] = m[taskId];
+        m[taskId] = {x, newPriority};
+        S.extract({z, taskId, x});
+        S.insert({newPriority, taskId, x});
     }
-
-    void rmv(int y) {
-        auto [x, z] = mp2[y];
-        mp2.erase(y);
-        S.extract({z, y, x});
+    
+    void rmv(int taskId) {
+        auto [x, z] = m[taskId];
+        m.erase(taskId);
+        S.extract({z, taskId, x});
     }
-
+    
     int execTop() {
-        if (S.empty()) return -1;
-        auto [z, y, x] = *S.rbegin();
+        if(S.empty()) return -1;
+        auto [z, y, x] = *S.rbegin(); // the iterator need to dereference, and also rebegin gives top
         rmv(y);
         return x;
     }
 };
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
