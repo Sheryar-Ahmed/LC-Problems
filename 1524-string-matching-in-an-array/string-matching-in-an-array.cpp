188990a79206ee6ff518b287bf751658dd69abaa
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-01-07 12:59:04 +0500

    Time: 18 ms (6.83%) | Memory: 18.3 MB (5.35%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
-    bool isExist(const string& pattern, const string& text) {
-        // Check if `pattern` is a substring of `text`
-        return text.find(pattern) != string::npos;
+bool isExist(const string& pattern, const string& text) {
+    int m = pattern.size(), n = text.size();
+    vector<int> lps(m, 0); // Longest prefix-suffix array
+
+    // Build the LPS array
+    for (int i = 1, len = 0; i < m;) {
+        if (pattern[i] == pattern[len]) {
+            lps[i++] = ++len;
+        } else if (len > 0) {
+            len = lps[len - 1];
+        } else {
+            lps[i++] = 0;
+        }
     }
+
+    // Perform KMP search
+    for (int i = 0, j = 0; i < n;) {
+        if (pattern[j] == text[i]) {
+            i++, j++;
+        }
+        if (j == m) {
+            return true; // Found pattern in text
+        }
+        if (i < n && pattern[j] != text[i]) {
+            if (j > 0) j = lps[j - 1];
+            else i++;
+        }
+    }
+
+    return false;
+}
 
     vector<string> stringMatching(vector<string>& words) {
         vector<string> ans;
