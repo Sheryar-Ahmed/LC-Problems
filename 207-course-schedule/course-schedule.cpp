3aae8d6cd77e518ae5310e81f66cba8f2ec8efa5
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-03-30 00:24:16 +0500

    Time: 4 ms (71.77%) | Memory: 20.1 MB (34.72%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    bool topologicalSortCheck(unordered_map<int, vector<int>> adj, int n, vector<int> indegree) {
-        queue<int> q;
-        int count = 0; // kitne nodes mane viist krliey hai
-        // those whose indgree is zero is pushed to the q
-        for(int i=0 ;i < n; i++) {
-            if(indegree[i]  == 0) {q.push(i); count++;};
-        }
-
-        while(!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            for(auto v: adj[u]){
-                indegree[v]--;
-                if(indegree[v] == 0) {
-                    q.push(v);
-                    count++;
-                }
+    bool isCycleDFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion){
+        visited[u] = true;
+        inRecursion[u] = true; // remember example of 1--->2<---3 current recursion stack thing
+        for(auto v: adj[u]){
+            if(!visited[v] && isCycleDFS(adj, v, visited, inRecursion)){
+                return true; // cycle found
+            }else if(inRecursion[v]){
+                return true; // if its not visited but we reached the current recursion again which means we are in cycle
             }
         }
-        return count == n;
+        inRecursion[u] = false; // we cannot call dfs further so we return our stack.
+        return false; // no cycle found
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // with dfs
         unordered_map<int, vector<int>> adj;
-        vector<int> inDegree(numCourses, 0); // kahn's algo
-
-        for(auto &vec: prerequisites) {
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
+        for(auto &vec: prerequisites){
             int a = vec[0];
             int b = vec[1];
-            // b --> a
             adj[b].push_back(a);
-            // count degree 'a' mei ja rha ha b say
-            inDegree[a]++;
         }
 
-        return topologicalSortCheck(adj, numCourses, inDegree);
-
+        for(int i=0; i < numCourses; i++){
+            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion)){
+                return false;
+            }
+        }
+        return true;
     }
 };
