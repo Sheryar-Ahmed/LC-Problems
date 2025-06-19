4e953ef1eb8c5063dcd9fe9459c6b5c037e63a64
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-06-20 02:11:47 +0500

    Time: 1 ms (45.92%) | Memory: 62.9 MB (78.62%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
         int i = 0;
-        int j = n-1;
-        int maxAr = 0;
-        while(i < j){
-            //calculate which height is smaller
+        int j = height.size()-1;
+        int maxArea = INT_MIN;
+        while( i < j){
             int h = min(height[i], height[j]);
-            int width = j-i;
-            int area = h*width;
-            maxAr = max(maxAr, area);
-            // question when to move left and right pointer. move left if its shorter than right because ultimately the shorter height will give us more results rather than taller.
+            int w = j-i;
+            maxArea = max(maxArea, h*w);
             if(height[i] < height[j]){
                 i++;
-            }else {
+            }else{
                 j--;
             }
         }
-        return maxAr;
+        return maxArea;
     }
 };
