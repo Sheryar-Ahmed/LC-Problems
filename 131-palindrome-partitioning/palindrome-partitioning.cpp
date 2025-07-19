4db2ec3773adefd0f7a53815ff19f53352c3bfac
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-19 03:34:17 -0700

    Time: 32 ms (82.84%) | Memory: 53.5 MB (85.11%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -3,25 +3,29 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> pars;
         vector<string> par;
-        partition(s, 0, par, pars);
+        backtrack(s, 0, par, pars);
         return pars;
     }
-private: 
-    void partition(string& s, int start, vector<string>& par, vector<vector<string>>& pars) {
+
+private:
+    void backtrack(string& s, int start, vector<string>& par,
+                   vector<vector<string>>& pars) {
         int n = s.length();
-        if(start==n){
+        if (start == n) {
             pars.push_back(par);
-        }else{
-            for(int i = start ; i < n ;i ++){
-               if(isPalindrome(s,start,i)){
-                   par.push_back(s.substr(start,i-start+1));
-                   partition(s,i+1,par,pars);
-                   par.pop_back();
-               }
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            // i is end of the string and start is start of the string so we are cutting 
+            if (isPalindrome(s, start, i)) {
+                par.push_back(s.substr(start, i - start + 1)); // cut the string
+                backtrack(s, i + 1, par, pars); // increment the length of the string
+                par.pop_back(); // backtrack
             }
         }
     }
-    
+
     bool isPalindrome(string& s, int l, int r) {
         while (l < r) {
             if (s[l++] != s[r--]) {
