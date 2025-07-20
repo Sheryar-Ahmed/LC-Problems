19d75c79083309751059d3efadca9ab64284f7fc
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-20 13:38:55 -0700

    Time: 15 ms (72.52%) | Memory: 26 MB (55.95%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // need to create hashmap with sorting the word and link the matching one's with it
-        // just iterate over the hashmap and based on the key index which is sorted just add a vector and store the results and return them
         unordered_map<string, vector<string>> mp;
-        for(auto str: strs){
+        for(auto &str: strs){
             string temp = str;
             sort(temp.begin(), temp.end());
             mp[temp].push_back(str);
         }
-        vector<vector<string>> result;
-        for(auto key: mp){
-            result.push_back(key.second);
+        vector<vector<string>> ans;
+        for(auto it: mp){
+            ans.push_back(it.second);
         }
-        return result;
+        return ans;
     }
 };
