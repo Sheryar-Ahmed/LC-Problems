7d2e4803647b7322072355c7e0acebd37c89063f
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-20 13:03:44 -0700

    Time: 4 ms (39.27%) | Memory: 9.7 MB (48.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() < t.size()){
-            return false;
-        }else if(s.size() > t.size()){
-            return false;
-        }
-
-        unordered_map<char, int> mp;
-        for(auto &c: s){
-            mp[c]++; // count the frequencies
-        }
-
-        for(auto &c: t){
-            if(mp.find(c) == mp.end() || mp[c] == 0){ // if not found or found but the first string does not have it.
-                return false;
-            }
-            mp[c]--; // decrement by 1 if found;
-        }
-        return true;
+        // sorting || counting by hashmap
+        // for(int i=0; i < s.size(); i++){
+            
+        // }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
