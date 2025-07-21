f010004ef5cba4b39c83c7a648a483f46c2bce89
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-21 07:02:39 -0700

    Time: 52 ms (72.02%) | Memory: 79 MB (52.83%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        // divide 
-        // sort
-        // merge
-        // simple technique is merge sort
-        mergeSort(nums, 0, nums.size()-1); // pass the start and end
-        return nums;
-    }
-private:
-    void mergeSort(vector<int> & nums, int l, int r){
-        if(l < r) {
-            int mid = l+(r-l)/2;
-            mergeSort(nums, l, mid);
-            mergeSort(nums, mid+1, r);
-
-            // merge the left and right arrays
-            merge(nums, l, mid, r);
+        // we can use priority_queue 
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto &x: nums){
+            pq.push(x);
         }
-    }
-    void merge(vector<int>& nums, int l, int m, int r){
-        int i = l;
-        int j = m+1;
-        int size = r-l+1;
-        vector<int> temp; // temporary array to store the sorted results of two split arrays
-        while(i <= m && j <= r){
-            if(nums[i] < nums[j]) temp.push_back(nums[i++]);
-            else temp.push_back(nums[j++]);
+        vector<int> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
         }
-        // remining elements in the left part
-        // remaining elements in the right part
-        while(i <= m ) temp.push_back(nums[i++]);
-        while(j <= r) temp.push_back(nums[j++]);
-
-        // now we have sorted the elements now copy back to the original array
-        for(int c=0; c < size; c++){
-            nums[l+c] = temp[c];
-        }
+        return ans;
     }
 };
