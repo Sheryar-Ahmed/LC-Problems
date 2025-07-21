ec31393820a21c70ef5c705bd900d6dd3bb4ab15
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-21 06:40:08 -0700

    Time: 4 ms (19.79%) | Memory: 28.2 MB (26.73%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
         unordered_map<int, int> freq;
         for(auto &x: nums){
             freq[x]++;
+            if(freq[x] > n / 2) return x;
         }
-        for(auto &it: freq){
-            if(it.second > n / 2) return it.first;
-        }
+        // for(auto &it: freq){
+        //     if(it.second > n / 2) return it.first;
+        // }
         return -1;
     }
 };
