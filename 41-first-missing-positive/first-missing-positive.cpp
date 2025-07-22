8453f655df1f17d9500bdd5c5dae78365c85dd2d
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-22 07:36:24 -0700

    Time: 5 ms (36.81%) | Memory: 55.1 MB (47.30%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> seen(n, false);
+        sort(nums.begin(), nums.end());
+        int missing = 1;
         for(auto &x: nums){
-            if(x > 0 && x <= n){
-                seen[x-1] = true;
+            if( x > 0 && x == missing){
+                missing++;
             }
         }
-        for(int i=0;i  < seen.size(); i++){
-            if(!seen[i]) return i+1;
-        }
-        return n+1;
+        return missing;
+        // int n = nums.size();
+        // vector<bool> seen(n, false);
+        // for(auto &x: nums){
+        //     if(x > 0 && x <= n){
+        //         seen[x-1] = true;
+        //     }
+        // }
+        // for(int i=0;i  < seen.size(); i++){
+        //     if(!seen[i]) return i+1;
+        // }
+        // return n+1;
         // int missing = 1;
         // while(true){
         //     bool flag = true;
