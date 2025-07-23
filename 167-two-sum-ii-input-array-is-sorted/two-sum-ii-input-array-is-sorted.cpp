09d7fe5d9c32c4b6f319a5283d2f96b0cca2863b
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-23 03:30:18 -0700

    Time: 5 ms (2.35%) | Memory: 19.6 MB (31.72%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i = 0;
-        int j = numbers.size()-1;
-        while(i < j){
-            if(numbers[i]+numbers[j] == target){
-                return {i+1, j+1};
-            } else if (numbers[i]+numbers[j] < target){
-                i++;
-            }else{
-                j--;
+        int n = numbers.size();
+        for (int i = 0; i < n - 1; i++) {
+            int complement = target - numbers[i];
+            int L = i + 1, R = n - 1;
+            while (L <= R) {
+                int mid = L + (R - L) / 2;
+                if (numbers[mid] == complement) {
+                    return {i + 1, mid + 1}; // 1-based indexing
+                } else if (numbers[mid] < complement) {
+                    L = mid + 1;
+                } else {
+                    R = mid - 1;
+                }
             }
         }
-        return {};
+        return {-1, -1}; // Shouldn't happen if one valid pair is guaranteed
     }
 };
