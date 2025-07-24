a2209153745a4556153eaf42c2e560d8d4cd66a5
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-24 11:08:05 -0700

    Time: 19 ms (81.56%) | Memory: 17.5 MB (58.05%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
         int n = nums.size();
 
-        for (int i = 0; i < n - 3; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue; // skip duplicates
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;  // skip duplicates for i
 
-            for (int j = i + 1; j < n - 2; ++j) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue; // skip duplicates
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;  // skip duplicates for j
 
-                int left = j + 1, right = n - 1;
+                int L = j + 1;
+                int R = n - 1;
 
-                while (left < right) {
-                    long long sum =
-                        (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                while (L < R) {
+                    long long sum = 1LL * nums[i] + nums[j] + nums[L] + nums[R];  // to avoid overflow
 
-                    if (sum < target) {
-                        left++;
-                    } else if (sum > target) {
-                        right--;
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[L], nums[R]});
+                        while (L < R && nums[L] == nums[L + 1]) L++;
+                        while (L < R && nums[R] == nums[R - 1]) R--;
+                        L++;
+                        R--;
+                    } else if (sum < target) {
+                        L++;
                     } else {
-                        res.push_back(
-                            {nums[i], nums[j], nums[left], nums[right]});
-
-                        while (left < right && nums[left] == nums[left + 1])
-                            left++; // skip dups
-                        while (left < right && nums[right] == nums[right - 1])
-                            right--; // skip dups
-
-                        left++;
-                        right--;
+                        R--;
                     }
                 }
             }
         }
-
-        return res;
+        return ans;
     }
 };
