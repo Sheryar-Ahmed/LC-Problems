1aea1d8d059c322199435bee212666f7be320fdb
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-24 12:35:04 -0700

    Time: 23 ms (20.17%) | Memory: 45.7 MB (95.86%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        // Sort the people weights in ascending order
         sort(people.begin(), people.end());
-
-        int i = 0;
-        int j = people.size() - 1;
-        int count = 0;
-
-        while (i <= j) {
-            int sum = people[i] + people[j];
-
-            // If the lightest and heaviest person can share a boat
-            if (sum <= limit) {
-                i++; // include lighter person
-                j--; // include heavier person
-            } else {
-                // Only the heavier person can go (they're too heavy to pair)
-                j--;
+        int boat = 0;
+        int L = 0;
+        int R = people.size() - 1;
+        while (L <= R) {
+            // R will always go either with left or without it
+            if(people[L]+people[R] <= limit){
+                L++; // left will got with right
             }
-
-            // In either case, we used a boat
-            count++;
+            R--;
+            boat++;
         }
-
-        return count;
+        return boat;
     }
 };
