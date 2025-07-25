4c43f0d1a04f1decc9e8312d4cec18b7e19f9840
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-25 08:49:04 -0700

    Time: 2020 ms (5.06%) | Memory: 136.8 MB (96.51%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        deque<int> dq; // Stores indices of elements in decreasing order of value
+        int L = 0;
+        int maxIndex = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            // Remove elements that are out of the current window
-            if (!dq.empty() && dq.front() < i - k + 1) {
-                dq.pop_front();
+        // Find initial max index
+        for (int i = 0; i < k; i++) {
+            if (nums[i] >= nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        ans.push_back(nums[maxIndex]);
+
+        for (int R = k; R < nums.size(); R++) {
+            L++; // move window
+
+            // Check if maxIndex is out of window
+            if (maxIndex < L) {
+                // Recompute max in window
+                maxIndex = L;
+                for (int i = L; i <= R; i++) {
+                    if (nums[i] >= nums[maxIndex]) {
+                        maxIndex = i;
+                    }
+                }
+            } else if (nums[R] >= nums[maxIndex]) {
+                // New max enters window
+                maxIndex = R;
             }
 
-            // Remove elements that are smaller than the current element
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
-
-            // Add the current element's index to the deque
-            dq.push_back(i);
-
-            // If the window has reached size `k`, add the maximum to the result
-            if (i >= k - 1) {
-                ans.push_back(nums[dq.front()]);
-            }
+            ans.push_back(nums[maxIndex]);
         }
 
         return ans;
