28ac3992d39625d9822b6de9078bd1daa4a55792
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-25 05:56:25 -0700

    Time: 77 ms (44.58%) | Memory: 93.8 MB (77.38%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int l = 0;
-        set<int> window;
-        for(int r = 0; r < nums.size(); r++){
-            // we have to set the window
-            if(r-l > k){
+        unordered_set<int> window;
+        int l =0;
+        for(int r=0; r < nums.size();r++){
+            while(r-l > k){
                 window.erase(nums[l]);
                 l++;
             }
-            if(window.find(nums[r]) != window.end()){ // we found a duplicate in the window
+            if(window.find(nums[r]) != window.end()){
                 return true;
             }
             window.insert(nums[r]);
         }
         return false;
-
     }
 };
