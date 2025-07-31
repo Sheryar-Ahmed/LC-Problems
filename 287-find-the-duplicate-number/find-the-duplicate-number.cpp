27ce0e265c033cf941cd924c6523d64d070527ec
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-07-31 03:48:28 -0700

    Time: 0 ms (100.00%) | Memory: 64.9 MB (99.29%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = 0;
-        int fast = 0;
+        int slow = nums[0];
+        int fast = nums[0];
         do{
             slow = nums[slow];
             fast = nums[nums[fast]]; // why now just fast because if i do both has initial 0 will be equal
         }while(slow != fast);
         // found intersection
-        int slow2 = 0;
-        while(slow2 != slow){
+        slow = nums[0];
+        while(fast != slow){
             slow = nums[slow];
-            slow2 = nums[slow2];
+            fast = nums[fast];
         }
         return slow;
     }
