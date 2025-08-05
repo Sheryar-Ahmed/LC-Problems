c32f386fa2548eecd7b90ffa81bfa05d4483898f
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-05 00:14:04 -0700

    Time: 0 ms (100.00%) | Memory: 22.1 MB (17.64%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,15 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+TreeNode* prev = nullptr;
 public:
-    bool isValidBST(TreeNode* root, long long minVal = LLONG_MIN, long long maxVal = LLONG_MAX) {
-        if (!root) {
-            return true;
-        }
-        if (root->val <= minVal || root->val >= maxVal) {
-            return false;
-        }
-// The left subtree is constrained by the maximum value being the parent node's value.
-// The right subtree is constrained by the minimum value being the parent node's value.
-        return isValidBST(root->left, minVal, root->val) &&
-               isValidBST(root->right, root->val, maxVal);
+    bool isValidBST(TreeNode* root) {
+        return inorder(root);
+    }
+    bool inorder(TreeNode* root){
+        if(!root) return true;
+        if(!inorder(root->left)) return false;
+        if(prev && root->val <= prev->val) return false;
+        prev = root;
+        if(!inorder(root->right)) return false;
+        return true;
     }
 };
