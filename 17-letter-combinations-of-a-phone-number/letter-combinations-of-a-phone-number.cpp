ae5f92c2d6081774534c239f606cc1fba1536f35
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-13 01:34:42 -0700

    Time: 2 ms (16.86%) | Memory: 9.7 MB (15.31%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    vector<string> result;
-
-    void backtrack(int idx, string &digits, unordered_map<char, string> &mp, string &temp) {
-        if (idx >= digits.length()) {
-            result.push_back(temp);
-            return;
-        }
-        char ch = digits[idx];
-        string str = mp[ch];
-        for (int i = 0; i < str.length(); i++) {
-            // do
-            temp.push_back(str[i]);
-            // explore
-            backtrack(idx + 1, digits, mp, temp);
-            // backtrack
-            temp.pop_back();
-        }
-    }
+    vector<string> res;
+    vector<string> digitToChar = {"", "", "abc", "def", "ghi", "jkl",
+                                  "mno", "qprs", "tuv", "wxyz"};
 
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) {
-            return {};
-        }
-                
-        unordered_map<char, string> mp = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},
-            {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
-        };
+        if (digits.empty()) return res;
+        backtrack(0, "", digits);
+        return res;
+    }
 
-        string temp = "";
-        backtrack(0, digits, mp, temp);
-        return result;
+    void backtrack(int i, string curStr, string &digits) {
+        if (curStr.size() == digits.size()) {
+            res.push_back(curStr);
+            return;
+        }
+        string chars = digitToChar[digits[i] - '0'];
+        for (char c : chars) {
+            backtrack(i + 1, curStr + c, digits);
+        }
     }
 };
