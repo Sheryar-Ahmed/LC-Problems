d67179caf7b74e3c70f14282a59ccf4936ad9e9c
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-14 07:58:56 -0700

    Time: 323 ms (80.67%) | Memory: 563.3 MB (57.19%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -3,35 +3,32 @@ public:
     bool isWord;
     TrieNode* childNode[26];
     TrieNode() {
-        isWord = false;
         for (int i = 0; i < 26; i++) {
             childNode[i] = nullptr;
         }
+        isWord = false;
     }
 };
 
 class WordDictionary {
 private:
     TrieNode* root;
-
-    bool dfs(TrieNode* node, const string& word, int index) {
-        if (!node) return false;
-        if (index == word.size()) return node->isWord;
-
+    bool dfs(TrieNode* root, string& word, int index){
+        if(!root) return false;
+        if(index == word.size()) return root->isWord;
         char ch = word[index];
-        if (ch == '.') {  // Wildcard case
-            for (int i = 0; i < 26; i++) {
-                if (dfs(node->childNode[i], word, index + 1)) {
+        if(ch == '.'){
+            for(int i=0; i< 26; i++){
+                if(dfs(root->childNode[i], word, index+1)){
                     return true;
                 }
             }
             return false;
-        } else {  // Regular character case
-            int idx = ch - 'a';
-            return dfs(node->childNode[idx], word, index + 1);
+        }else{
+            int idx = ch-'a';
+            return dfs(root->childNode[idx], word, index+1);
         }
     }
-
 public:
     WordDictionary() {
         root = new TrieNode();
@@ -39,9 +36,9 @@ public:
 
     void addWord(string word) {
         TrieNode* crawler = root;
-        for (char ch : word) {
-            int idx = ch - 'a';
-            if (!crawler->childNode[idx]) {
+        for(auto s: word){
+            int idx = s-'a';
+            if(crawler->childNode[idx] == nullptr){
                 crawler->childNode[idx] = new TrieNode();
             }
             crawler = crawler->childNode[idx];
@@ -50,6 +47,13 @@ public:
     }
 
     bool search(string word) {
-        return dfs(root, word, 0);
+        return dfs(root, word, 0); // root, currentword, starting index;
     }
 };
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
