f380ca8ecd791f2a04ca6fe2e25e9aec12edac5b
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-18 07:11:17 -0700

    Time: 4 ms (81.39%) | Memory: 24.9 MB (31.65%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m = heights.size();
-        int n = heights[0].size();
-        //store the atlantic and pacific values
-        vector<vector<bool>> pacific(m, vector<bool>(n));
-        vector<vector<bool>> atlantic(m, vector<bool>(n));
-        for(int i=0; i < m; i++){
-            dfs(heights, pacific,i, 0, m, n);
-            dfs(heights, atlantic, i, n-1, m, n);
-        }
-        for(int j=0; j < n; j++){
-            dfs(heights, pacific,0, j, m, n);
-            dfs(heights, atlantic,m-1, j, m, n);
-        }
+        int rows = heights.size();
+        int cols = heights[0].size();
+
+        vector<vector<int>> pac(rows, vector<int>(cols, 0));
+        vector<vector<int>> atl(rows, vector<int>(cols, 0));
+
+        // Directions
+        vector<vector<int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // DFS lambda
+        function<void(int,int,vector<vector<int>>&)> dfs = [&](int r, int c, vector<vector<int>>& ocean) {
+            ocean[r][c] = 1;
+            for (auto &d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr < 0 || nc < 0 || nr >= rows || nc >= cols) continue;
+                if (ocean[nr][nc]) continue; // already visited
+                if (heights[nr][nc] < heights[r][c]) continue; // must go uphill or flat
+                dfs(nr, nc, ocean);
+            }
+        };
+
+        // Pacific: top row + left col
+        for (int c = 0; c < cols; c++) dfs(0, c, pac);
+        for (int r = 0; r < rows; r++) dfs(r, 0, pac);
+
+        // Atlantic: bottom row + right col
+        for (int c = 0; c < cols; c++) dfs(rows-1, c, atl);
+        for (int r = 0; r < rows; r++) dfs(r, cols-1, atl);
+
+        // Collect results
         vector<vector<int>> res;
-        for(int i=0; i < m; i++){
-            for(int j=0; j < n; j++){
-                if(pacific[i][j] && atlantic[i][j]){
-                    res.push_back({i,j});
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (pac[r][c] && atl[r][c]) {
+                    res.push_back({r, c});
                 }
             }
         }
         return res;
-    }
-private:
-    void dfs(vector<vector<int>> & heights, vector<vector<bool>>& visited, int i, int j, int m, int n){
-        visited[i][j] = true;
-        if(i > 0 && !visited[i-1][j] && heights[i-1][j] >= heights[i][j]){
-            dfs(heights, visited, i-1, j, m, n);
-        }
-        if(i < m-1 && !visited[i+1][j] && heights[i+1][j] >= heights[i][j]){
-            dfs(heights, visited, i+1, j, m, n);
-
-        }
-        if(j > 0 && !visited[i][j-1] && heights[i][j-1] >= heights[i][j]){
-            dfs(heights, visited, i, j-1, m, n);
-
-        }
-        if(j < n-1 && !visited[i][j+1] && heights[i][j+1] >= heights[i][j]){
-            dfs(heights, visited, i, j+1, m, n);
-        }
     }
 };
