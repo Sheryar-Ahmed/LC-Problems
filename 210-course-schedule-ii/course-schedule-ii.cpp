b8c4e68783e44bb1be1f429a2d4ebad3a2a60cfe
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-18 23:50:55 -0700

    Time: 2 ms (78.35%) | Memory: 18.5 MB (51.29%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,46 +1,48 @@
 class Solution {
-public:
+private:
     vector<int> topologicalSort(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree){
         vector<int> ans;
         queue<int> q;
         int count = 0;
-        // pushing all those have zero degree
-        for(int i=0; i < n; i++){
-            if(indegree[i] == 0){
+
+        // Step 1: start with nodes having indegree 0
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
                 ans.push_back(i);
-                q.push(i); 
+                q.push(i);
                 count++;
             }
         }
-
-        while(!q.empty()){
+        while (!q.empty()) {
             int u = q.front();
             q.pop();
-            for(auto &v: adj[u]){
-                indegree[v]--;
-                if(indegree[v] == 0){
+
+            for (auto& v : adj[u]) {
+                indegree[v]--;          // one prereq is done
+                if (indegree[v] == 0) { // now course is free to take
                     ans.push_back(v);
                     q.push(v);
                     count++;
                 }
             }
         }
-        if(count == n){
+        if (count == n) {
             return ans;
-        }else{
+        } else {
             return {};
         }
-    }
+    };
+
+public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // kahn's algo
         unordered_map<int, vector<int>> adj;
         vector<int> indegree(numCourses, 0);
 
-        for(auto &vec: prerequisites){
+        for (auto& vec : prerequisites) {
             int a = vec[0];
             int b = vec[1];
-            adj[b].push_back(a);
-            indegree[a]++;
+            adj[b].push_back(a); // b -> a (b must be done before a)
+            indegree[a]++;       // a has one more prerequisite
         }
 
         return topologicalSort(adj, numCourses, indegree);
