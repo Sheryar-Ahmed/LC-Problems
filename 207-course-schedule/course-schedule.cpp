8cbf2b6dd89257afbc9338bf1ff187f9f07f0288
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-19 00:02:16 -0700

    Time: 7 ms (48.95%) | Memory: 20.1 MB (31.44%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,30 @@
 class Solution {
-public:
-    bool isCycleDFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion){
-        visited[u] = true;
-        inRecursion[u] = true; // remember example of 1--->2<---3 current recursion stack thing
-        for(auto v: adj[u]){
-            if(!visited[v] && isCycleDFS(adj, v, visited, inRecursion)){
-                return true; // cycle found
-            }else if(inRecursion[v]){
-                return true; // if its not visited but we reached the current recursion again which means we are in cycle
-            }
-        }
-        inRecursion[u] = false; // we cannot call dfs further so we return our stack.
-        return false; // no cycle found
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // with dfs
-        unordered_map<int, vector<int>> adj;
-        vector<bool> visited(numCourses, false);
-        vector<bool> inRecursion(numCourses, false);
-        for(auto &vec: prerequisites){
-            int a = vec[0];
-            int b = vec[1];
-            adj[b].push_back(a);
-        }
-
-        for(int i=0; i < numCourses; i++){
-            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion)){
+private:
+bool dfs(int node, unordered_map<int, vector<int>> &adj, int n, vector<int>& state){
+    state[node] = 1; // set to visiting
+    for(auto nei: adj[node]){
+        if(state[nei] == 1) return false; // two can't be in process which means there is a cycle
+        if(state[nei] == 0) {
+            if(!dfs(nei, adj, n, state)){
                 return false;
+            }
+        }        
+    }
+    state[node] = 2;
+    return true;
+}
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        for(auto &p: prerequisites){
+            adj[p[1]].push_back(p[0]); // b --> a
+        }
+        vector<int> state(numCourses, 0); // 0 univisted, 1 visiting,2 visited;
+        for(int i=0; i < numCourses; i++){
+            if(state[i] == 0){
+                if(!dfs(i, adj, numCourses, state)){
+                    return false;
+                }
             }
         }
         return true;
