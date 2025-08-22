7086fa9529dbd9426482f9f62edda1c1b121172e
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-22 13:21:44 -0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 0 or n== 1) return 1;
-        vector<int> dp(n+1);
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2; i <= n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        vector<int> memo(n+1, -1);
+        return solve(memo, n);
+    }
+    int solve (vector<int>& memo, int n){
+        if(n < 2) return 1;
+        if(memo[n] != -1) return memo[n];
+        return memo[n] = solve(memo, n-1)+solve(memo, n-2); // febonacci related problem
     }
 };
