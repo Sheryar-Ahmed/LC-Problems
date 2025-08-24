5c9f88cf3bb848b88c59b02f24bc073e58582be1
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-08-24 11:47:47 -0700

    Time: 0 ms (100.00%) | Memory: 10.8 MB (40.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int robLinear(vector<int>& nums, int start, int end) {
-        int len = end - start + 1;
-        if (len == 1) return nums[start];
-        
-        vector<int> dp(len);
-        dp[0] = nums[start];
-        dp[1] = max(nums[start], nums[start + 1]);
+    int robFrom(int i, int end, vector<int>& nums, vector<int>& memo) {
+        if (i > end) return 0;
+        if (memo[i] != -1) return memo[i];
 
-        for (int i = 2; i < len; i++) {
-            dp[i] = max(nums[start + i] + dp[i - 2], dp[i - 1]);
-        }
-
-        return dp[len - 1];
+        return memo[i] = max(nums[i] + robFrom(i + 2, end, nums, memo),
+                             robFrom(i + 1, end, nums, memo));
     }
 
     int rob(vector<int>& nums) {
@@ -20,6 +13,12 @@ public:
         if (n == 0) return 0;
         if (n == 1) return nums[0];
 
-        return max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
+        vector<int> memo1(n, -1);  // rob from 0 to n-2
+        vector<int> memo2(n, -1);  // rob from 1 to n-1
+
+        int rob1 = robFrom(0, n - 2, nums, memo1);  // exclude last
+        int rob2 = robFrom(1, n - 1, nums, memo2);  // exclude first
+
+        return max(rob1, rob2);
     }
 };
