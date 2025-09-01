d0e8e482f37871c97896b6d9902abb6f34c741d6
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-01 12:30:44 +0500

    Time: 3 ms (29.22%) | Memory: 52.4 MB (34.67%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-enum State { GOOD, BAD, UNKNOWN };
-
-bool canJumpFrom(int pos, vector<int>& nums, vector<State>& memo) {
-    if (memo[pos] != UNKNOWN) return memo[pos] == GOOD;
-
-    int furthest = min((int)nums.size() - 1, pos + nums[pos]);
-    for (int next = pos + 1; next <= furthest; next++) {
-        if (canJumpFrom(next, nums, memo)) {
-            memo[pos] = GOOD;
-            return true;
-        }
-    }
-    memo[pos] = BAD;
-    return false;
-}
-
 bool canJump(vector<int>& nums) {
-    vector<State> memo(nums.size(), UNKNOWN);
-    memo[nums.size() - 1] = GOOD;
-    return canJumpFrom(0, nums, memo);
+    int farthest = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > farthest) return false;
+        farthest = max(farthest, i + nums[i]);
+    }
+    return true;
 }
 };
