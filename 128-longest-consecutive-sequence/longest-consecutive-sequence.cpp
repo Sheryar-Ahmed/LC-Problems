c672c80f570344103133bde093267ed26827e140
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-07 16:42:58 +0500

    Time: 88 ms (37.02%) | Memory: 88.8 MB (65.39%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        int longest = 0;
-
-        for (int num : numSet) {
-            // Only start counting if this is the beginning of a sequence
-            if (numSet.find(num - 1) == numSet.end()) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (numSet.find(currentNum + 1) != numSet.end()) {
-                    currentNum += 1;
-                    currentStreak += 1;
+        if (nums.size() < 1)
+            return 0;
+        if (nums.size() == 1)
+            return 1;
+        // sort krlo
+        // 1, 2, 3, 4, 100, 200
+        sort(nums.begin(), nums.end(), [](const auto& a, const auto& b) {
+            return a < b; // asc a > b // desc
+        });
+        // to get the start of the sequence if a subtract -1 from it, that must
+        // not availble in the nums.
+        unordered_set<int> look(nums.begin(), nums.end()); // fast lookups;
+        int longest = 1;
+        // for each element check if its has longest string
+        int i = 0;
+        // 0, 1, 1, 2
+        while (i < nums.size()) {
+            int currentLong = 1;
+            // skip duplicates
+            // while(i > 0 && i < nums.size() && nums[i] == nums[i-1]) continue;
+            if (look.find(nums[i] - 1) == look.end()) {
+                while (i < nums.size()-1 && nums[i] + 1 == nums[i + 1]) {
+                    currentLong++;
+                    i++;
+                    while(i < nums.size()-1 && nums[i] == nums[i+1]) i++; // skip duplicates
                 }
-
-                longest = max(longest, currentStreak);
             }
+            i++;
+            longest = max(currentLong, longest);
         }
 
         return longest;
