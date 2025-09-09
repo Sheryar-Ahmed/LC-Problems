17f4fbe9078bbad337190421ed0047c76169511a
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-09 19:56:09 +0500

    Time: 15 ms (72.99%) | Memory: 45.8 MB (69.67%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(), people.end());
+        int l = 0;
+        int r = people.size()-1;
         int boat = 0;
-        int L = 0;
-        int R = people.size() - 1;
-        while (L <= R) {
-            // R will always go either with left or without it
-            if(people[L]+people[R] <= limit){
-                L++; // left will got with right
+        while(l <= r){
+            if(people[l]+people[r] <= limit){
+                l++;
             }
-            R--;
+            r--;
             boat++;
         }
         return boat;
