88fad875221ef81b68e2f0dbca53c394acbefa33
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-09 19:11:50 +0500

    Time: 0 ms (100.00%) | Memory: 27.3 MB (20.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0, right = height.size() - 1;
-        int leftMax = 0, rightMax = 0;
+        int n = height.size();
+        vector<int> maxLeft(n, 0);
+        vector<int> maxRight(n, 0);
+        // formula is hard to predict min(maxLeft, maxRight)-height[i];
+        // for each position store its maxLeft, and maxRight
+        int maxL = height[0];
+        maxLeft[0] = 0;
+        for (int i = 1; i < n; i++) {
+            maxL = max(maxL, height[i - 1]);
+            maxLeft[i] = maxL;
+        }
+        int maxR = height[n - 1];
+        maxRight[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            maxR = max(maxR, height[i + 1]);
+            maxRight[i] = maxR;
+        }
+        // for (auto& l : maxLeft) {
+        //     cout << l << endl;
+        // }
+        // cout << endl;
+        // for (auto& r : maxRight) {
+        //     cout << r << endl;
+        // }
         int water = 0;
-
-        while (left < right) {
-            if (height[left] < height[right]) {
-                // Left is the shorter boundary
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    water += leftMax - height[left]; // water trapped above this bar
-
-                left++;
-            } else {
-                // Right is the shorter boundary
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    water += rightMax - height[right]; // water trapped above this bar
-
-                right--;
+        for (int i = 0; i < n; i++) {
+            int contain = min(maxLeft[i], maxRight[i]) - height[i];
+            if (contain >= 0) {
+                water += contain;
             }
         }
-
         return water;
     }
 };
