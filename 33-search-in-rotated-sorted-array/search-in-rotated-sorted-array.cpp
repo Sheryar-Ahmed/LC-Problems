3a87ae55eb1ec12d68a5f8a995905fdaf777c899
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-12 19:21:53 +0500

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if(nums.size() == 1 && nums[0] != target) return -1;
-        unordered_map<int, int> mp;
-        for(int i=0; i< nums.size();i++){
-            mp[nums[i]] = i;
-            if(mp.find(target) != mp.end()){
-                return mp[target];
+        // we need to find the lowest starting point.
+        // either the left half is sorted or right half is sorted based on that
+        // icrement or decrement it
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                // left half is sorted;
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // rigth half is sorted;
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
