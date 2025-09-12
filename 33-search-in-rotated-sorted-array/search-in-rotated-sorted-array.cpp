eb8bafd7a9c781e0d4aa50451c4ece185ce61e76
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-12 18:08:09 +0500

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int L = 0;
-        int R = nums.size() - 1;
-
-        while (L <= R) {
-            int mid = L + (R - L) / 2;
-
-            // Check if the middle element is the target
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            // Determine if the left half is sorted
-            if (nums[L] <= nums[mid]) {
-                // Check if the target lies within the sorted left half
-                if (nums[L] <= target && target < nums[mid]) {
-                    R = mid - 1;
-                } else {
-                    L = mid + 1;
-                }
-            } 
-            // Otherwise, the right half must be sorted
-            else {
-                // Check if the target lies within the sorted right half
-                if (nums[mid] < target && target <= nums[R]) {
-                    L = mid + 1;
-                } else {
-                    R = mid - 1;
-                }
+        if(nums.size() == 1 && nums[0] != target) return -1;
+        unordered_map<int, int> mp;
+        for(int i=0; i< nums.size();i++){
+            mp[nums[i]] = i;
+            if(mp.find(target) != mp.end()){
+                return mp[target];
             }
         }
-
-        // Target not found
         return -1;
     }
 };
