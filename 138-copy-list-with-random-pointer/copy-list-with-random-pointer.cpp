3138d1d52823b7798f3c798930de1b558eb60ac8
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-16 10:34:56 +0500

    Time: 7 ms (52.87%) | Memory: 15.3 MB (26.31%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,20 +17,19 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // we can initally make the copy with just next and store the random in the hashmap and traverse hashamp and link our copy to the random node.
-        unordered_map<Node*, Node*> oldToCopy; // original node, copy new node just with value
+        if(!head) return nullptr;
+        unordered_map<Node*, Node*> old_to_new;
         Node* curr = head;
-        while(curr) {
-            oldToCopy[curr] = new Node(curr->val);
+        while(curr){
+            old_to_new[curr] = new Node(curr->val);
             curr = curr->next;
         }
         curr = head;
-        while(curr) {
-            Node* copy = oldToCopy[curr];
-            copy->next = oldToCopy[curr->next];
-            copy->random = oldToCopy[curr->random];
+        while(curr){
+            old_to_new[curr]->next = old_to_new[curr->next];
+            old_to_new[curr]->random = old_to_new[curr->random];
             curr = curr->next;
         }
-        return oldToCopy[head];
+        return old_to_new[head];
     }
 };
