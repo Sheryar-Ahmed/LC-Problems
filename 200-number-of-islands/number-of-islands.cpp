315a7728d619cd05a454f66221c941a245a39807
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-19 01:30:44 +0500

    Time: 28 ms (51.31%) | Memory: 16.7 MB (65.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,30 @@
 class Solution {
-private:
-void dfs(vector<vector<char>>& grid, int i, int j){
-    if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0'){
-        return;
-    }
-    grid[i][j] = '0';
-    dfs(grid, i+1, j);
-    dfs(grid, i-1, j);
-    dfs(grid, i, j+1);
-    dfs(grid, i, j-1);
-}
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        for(int i=0; i < grid.size(); i++){
-            for(int j=0; j < grid[0].size(); j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
-                        count++;
+        int number = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    dfs(i, j, rows, cols, grid, visited);
+                    number++;
                 }
             }
         }
-        return count;
+        return number;
+    }
+    void dfs(int i, int j, int& rows, int& cols, vector<vector<char>>& grid,
+             vector<vector<bool>>& visited) {
+        if (i < 0 || i >= rows || j < 0 || j >= cols || visited[i][j] ||
+            grid[i][j] == '0') {
+            return;
+        }
+        visited[i][j] = true;
+        dfs(i + 1, j, rows, cols, grid, visited);
+        dfs(i - 1, j, rows, cols, grid, visited);
+        dfs(i, j + 1, rows, cols, grid, visited);
+        dfs(i, j - 1, rows, cols, grid, visited);
     }
 };
