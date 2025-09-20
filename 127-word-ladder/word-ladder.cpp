4ec9f5fd6c4398ee0686efaf418c1183a57a3f13
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-21 00:39:08 +0500

    Time: 71 ms (50.11%) | Memory: 22.8 MB (22.85%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-    unordered_set<string> dict(wordList.begin(), wordList.end());
-    
-    if (dict.find(endWord) == dict.end()) return 0;
-
-    queue<pair<string, int>> q;
-    q.push({beginWord, 1});
-    
-    while (!q.empty()) {
-        auto [word, level] = q.front();
-        q.pop();
-        
-        if (word == endWord) return level;
-        
-        for (int i = 0; i < word.size(); i++) {
-            char original = word[i];
-            for (char c = 'a'; c <= 'z'; c++) {
-                word[i] = c;
-                if (dict.find(word) != dict.end()) {
-                    q.push({word, level + 1});
-                    dict.erase(word); // remove to avoid revisiting
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        // make set of wordList for fast access
+        unordered_set<string> dic(wordList.begin(), wordList.end());
+        if (dic.find(endWord) == dic.end()) {
+            return 0;
+        }
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1}); // intially the steps are 1 to reach beginWord
+        unordered_set<string> visited;
+        visited.insert(beginWord);
+        while (!q.empty()) {
+            auto [word, steps] = q.front();
+            q.pop();
+            if (word == endWord)
+                return steps;
+            // else change one character.
+            for (int i = 0; i < word.size(); i++) {
+                string temp = word;
+                for (char c = 'a'; c <= 'z'; c++) {
+                    temp[i] = c;
+                    if (dic.find(temp) != dic.end() && !visited.count(temp)) {
+                        visited.insert(temp);
+                        q.push({temp, steps + 1});
+                    }
                 }
             }
-            word[i] = original; // backtrack
         }
+        return 0;
     }
-    return 0;
-}
 };
