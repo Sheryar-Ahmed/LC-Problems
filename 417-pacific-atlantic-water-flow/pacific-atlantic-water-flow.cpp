a8ffdd5caa3bde19742005c0ff712e1a8bbc90a5
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-20 17:53:54 +0500

    Time: 13 ms (33.22%) | Memory: 25.6 MB (25.02%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,44 +1,46 @@
 class Solution {
+    void dfs(int i, int j, int rows, int cols, vector<vector<bool>>& reachable,
+             vector<vector<int>>& heights) {
+        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+                    reachable[i][j] = true;
+        for (auto& dir : dirs) {
+            int ni = i + dir.first;
+            int nj = j + dir.second;
+            if (ni < 0 || ni >= rows || nj < 0 || nj >= cols) {
+                continue;
+            }
+            if (reachable[ni][nj])
+                continue;
+            if (heights[ni][nj] < heights[i][j])
+                continue;
+            dfs(ni, nj, rows, cols, reachable, heights);
+        }
+    }
+
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int rows = heights.size();
         int cols = heights[0].size();
+        vector<vector<bool>> atlantic(rows, vector<bool>(cols));
+        vector<vector<bool>> pacific(rows, vector<bool>(cols));
+        for (int i = 0; i < rows; i++)
+            dfs(i, 0, rows, cols, pacific, heights);
+        for (int j = 0; j < cols; j++)
+            dfs(0, j, rows, cols, pacific, heights);
 
-        vector<vector<int>> pac(rows, vector<int>(cols, 0));
-        vector<vector<int>> atl(rows, vector<int>(cols, 0));
+        for (int i = 0; i < rows; i++)
+            dfs(i, cols - 1, rows, cols, atlantic, heights);
+        for (int j = 0; j < cols; j++)
+            dfs(rows - 1, j, rows, cols, atlantic, heights);
 
-        // Directions
-        vector<vector<int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        // DFS lambda
-        function<void(int,int,vector<vector<int>>&)> dfs = [&](int r, int c, vector<vector<int>>& ocean) {
-            ocean[r][c] = 1;
-            for (auto &d : dirs) {
-                int nr = r + d[0], nc = c + d[1];
-                if (nr < 0 || nc < 0 || nr >= rows || nc >= cols) continue;
-                if (ocean[nr][nc]) continue; // already visited
-                if (heights[nr][nc] < heights[r][c]) continue; // must go uphill or flat
-                dfs(nr, nc, ocean);
-            }
-        };
-
-        // Pacific: top row + left col
-        for (int c = 0; c < cols; c++) dfs(0, c, pac);
-        for (int r = 0; r < rows; r++) dfs(r, 0, pac);
-
-        // Atlantic: bottom row + right col
-        for (int c = 0; c < cols; c++) dfs(rows-1, c, atl);
-        for (int r = 0; r < rows; r++) dfs(r, cols-1, atl);
-
-        // Collect results
-        vector<vector<int>> res;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (pac[r][c] && atl[r][c]) {
-                    res.push_back({r, c});
+        vector<vector<int>> ans;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (atlantic[i][j] && pacific[i][j]) {
+                    ans.push_back({i, j});
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
