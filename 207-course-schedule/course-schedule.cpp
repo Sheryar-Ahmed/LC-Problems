db5773b376aa3992416e5b35f85e42f69c75275b
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-20 16:19:08 +0500

    Time: 11 ms (21.59%) | Memory: 19.2 MB (74.62%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,30 +1,34 @@
 class Solution {
-private:
-bool dfs(int node, unordered_map<int, vector<int>> &adj, int n, vector<int>& state){
-    state[node] = 1; // set to visiting
-    for(auto nei: adj[node]){
-        if(state[nei] == 1) return false; // two can't be in process which means there is a cycle
-        if(state[nei] == 0) {
-            if(!dfs(nei, adj, n, state)){
-                return false;
-            }
-        }        
-    }
-    state[node] = 2;
-    return true;
-}
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adj;
-        for(auto &p: prerequisites){
-            adj[p[1]].push_back(p[0]); // b --> a
+    bool dfs(int node, vector<int>& state, vector<vector<int>>& adj) {
+        // if the current node is already in state of visiting then cycle
+        // detected and we cannot finish courses so return false
+        if (state[node] == 1)
+            return false;
+        if (state[node] == 2)
+            return true; // we need to skip this one.
+        state[node] = 1;
+        for (auto& nei : adj[node]) {
+            if (!dfs(nei, state, adj))
+                return false;
         }
-        vector<int> state(numCourses, 0); // 0 univisted, 1 visiting,2 visited;
-        for(int i=0; i < numCourses; i++){
-            if(state[i] == 0){
-                if(!dfs(i, adj, numCourses, state)){
-                    return false;
-                }
+        state[node] = 2;
+        return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // DAG, indegree+dfs(recursive stack);
+        vector<vector<int>> adj(numCourses);
+        for (auto& vec : prerequisites) {
+            auto a = vec[0];
+            auto b = vec[1];
+            adj[b].push_back(a);
+        }
+        vector<int> state(
+            numCourses); // 0 unvi, 1 mean visitiin, 2 done visited.
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) {
+                if (!dfs(i, state, adj))
+                    return false; // cycle detected;
             }
         }
         return true;
