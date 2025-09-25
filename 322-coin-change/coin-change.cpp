ca3da53195f7c009b1e48ec2d68fc42c4e6eb7ed
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-26 00:42:46 +0500

    Time: 23 ms (71.18%) | Memory: 18 MB (73.44%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, 1e9); // large number instead of infinity
-        dp[0] = 0; // base case
-
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int c : coins) {
-                if (i - c >= 0) {
-                    dp[i] = min(dp[i], dp[i - c] + 1);
+            for (auto c : coins) {
+                if (i - c >= 0 && dp[i - c] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - c]+1);
                 }
             }
         }
-
-        return dp[amount] == 1e9 ? -1 : dp[amount];
+        return dp[amount] != INT_MAX ? dp[amount] : -1;
     }
 };
