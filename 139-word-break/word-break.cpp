05d3366d16e2115482f8f0f6f987602387fb076a
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-26 17:26:44 +0500

    Time: 12 ms (41.39%) | Memory: 16 MB (47.35%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        // we can solve using dp dp[i] defines the string can be broken at i. we can create a dp of size s and check if all the string can be broken or not
         int n = s.size();
-        vector<bool> dp(n + 1, false);
-        dp[0] = true;  // base case: empty string
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.count(s.substr(j, i - j))) {
+        unordered_set<string> dic(wordDict.begin(), wordDict.end());
+        vector<bool> dp(n, false);
+        dp[0] = true;// for empty string
+        for(int i=1; i <= n; i++){
+            for(int j=0; j < i; j++){
+                if(dp[j] && dic.count(s.substr(j, i-j))){
                     dp[i] = true;
-                    break; // no need to check further
+                    break; // no further need to check
                 }
             }
         }
