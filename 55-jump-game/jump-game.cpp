fccfd4c8647df2b2d1951ad65b3e16d11e0abef2
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-28 04:37:04 +0500

    Time: 604 ms (8.06%) | Memory: 53.3 MB (14.62%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // the main intuition is if my current index is greater than my farthest index i can reach we are stuck buddy
-        int farthest = 0;
-        for(int i=0; i < nums.size(); i++){
-            if(i > farthest) return false;
-            farthest = max(farthest, i+nums[i]);
+        int n = nums.size();
+        vector<bool> dp(n, false); 
+        dp[0] = true;  // starting position is always reachable
+
+        for (int i = 0; i < n; i++) {
+            if (!dp[i]) continue; // if i is not reachable, skip
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                dp[i + j] = true;
+            }
         }
-        return true;
+        return dp[n - 1];
     }
 };
