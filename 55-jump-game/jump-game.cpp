b4003d102f2b916d3ac8d11a9773303d2f9f3c07
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-29 01:42:24 +0500

    Time: 0 ms (100.00%) | Memory: 52.3 MB (35.11%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> dp(n, false); 
-        dp[0] = true;  // starting position is always reachable
-
-        for (int i = 0; i < n; i++) {
-            if (!dp[i]) continue; // if i is not reachable, skip
-            for (int j = 1; j <= nums[i] && i + j < n; j++) {
-                dp[i + j] = true;
-            }
+        int farthest = 0;
+        for(int i=0; i < nums.size(); i++){
+            if(farthest < i) return false;
+            farthest = max(farthest, i+nums[i]);
         }
-        return dp[n - 1];
+        return true;
     }
 };
