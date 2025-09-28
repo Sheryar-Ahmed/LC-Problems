54b076cd05ea5d3b00843bafb7340d746f034436
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-29 01:37:18 +0500

    Time: 7 ms (41.24%) | Memory: 23.9 MB (31.11%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // Step 1: sort by start time
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
-            return a[0] < b[0];
+        // sort it 
+        sort(intervals.begin(), intervals.end(), [](const auto&a, const auto&b){
+            return a[0] < b[0]; // ascending order
         });
-
+        // initially we put the intervals index 0 to the answer and check the last element
         vector<vector<int>> ans;
-        // Step 2: start with the first interval
         ans.push_back(intervals[0]);
+        for(int i=1; i < intervals.size(); i++){
+            int last = ans.back()[1];
+            int a = intervals[i][0];
+            int b = intervals[i][1];
 
-        // Step 3: iterate through all intervals
-        for (int i = 1; i < intervals.size(); i++) {
-            int lastEnd = ans.back()[1];   // end of the last interval in ans
-            int currStart = intervals[i][0];
-            int currEnd = intervals[i][1];
-
-            if (lastEnd >= currStart) {
-                // merge by updating the end of the last interval
-                ans.back()[1] = max(lastEnd, currEnd);
-            } else {
-                // no overlap, just add this interval
+            if(last >= a){
+                // merge
+                ans.back()[1] = max(last, b);
+            }else{
                 ans.push_back(intervals[i]);
             }
         }
-
         return ans;
     }
 };
