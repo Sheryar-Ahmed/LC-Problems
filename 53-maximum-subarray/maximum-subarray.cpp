6a2523b02d577460d46d80fdd44e42acdbb1c5ae
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-09-29 23:45:30 +0500

    Time: 3 ms (19.62%) | Memory: 74.5 MB (16.74%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // with dp i think its gonna be O(n2)
         int n = nums.size();
-        vector<int> dp(n); // largest sum at each index i
+        vector<int> dp(n, 0); // -2, -1, -1, 3, 3, 5, 6, 6
         dp[0] = nums[0];
-        int ans = nums[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+        int ans = nums[0]; // 6
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(nums[i], dp[i-1] + nums[i]);
             ans = max(ans, dp[i]);
         }
+
         return ans;
     }
 };
