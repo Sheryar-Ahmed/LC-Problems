9b2d6062da0e6499af610c495e4d3dfee22ed873
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-03 00:20:36 +0500

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.92%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // from each point i have two decisions either go down or left within bounds
-        // for the first row and column there is 1 one way to reach after that we have to update our dp with the above and left value and at the end just return dp[n][m];
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        //top row only 1 path
+        // left most column only 1 path
+        vector<vector<int>> dp(m, vector<int>(n));
         for(int i=0; i < n; i++){
             dp[0][i] = 1;
         }
-        for(int j =0; j < m; j++){
-            dp[j][0] = 1;
+        for(int i=0; i < m; i++){
+            dp[i][0] = 1;
         }
         for(int i=1; i < m; i++){
             for(int j=1; j < n; j++){
@@ -16,6 +16,5 @@ public:
             }
         }
         return dp[m-1][n-1];
-        
     }
 };
