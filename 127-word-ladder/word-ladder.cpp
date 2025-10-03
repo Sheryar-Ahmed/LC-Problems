ab4fbaff4e2302b2c81f76443a765e2b820ae30c
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-03 22:19:28 +0500

    Time: 80 ms (45.36%) | Memory: 19.9 MB (88.06%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
-        // make set of wordList for fast access
         unordered_set<string> dic(wordList.begin(), wordList.end());
-        if (dic.find(endWord) == dic.end()) {
+        int levels = 1;
+        // bfs
+        if (!dic.count(endWord))
             return 0;
-        }
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1}); // intially the steps are 1 to reach beginWord
-        unordered_set<string> visited;
-        visited.insert(beginWord);
+        queue<string> q;
+        q.push(beginWord);
         while (!q.empty()) {
-            auto [word, steps] = q.front();
-            q.pop();
-            if (word == endWord)
-                return steps;
-            // else change one character.
-            for (int i = 0; i < word.size(); i++) {
-                string temp = word;
-                for (char c = 'a'; c <= 'z'; c++) {
-                    temp[i] = c;
-                    if (dic.find(temp) != dic.end() && !visited.count(temp)) {
-                        visited.insert(temp);
-                        q.push({temp, steps + 1});
+            int size = q.size();
+            while (size-- && !q.empty()) {
+                string word = q.front();
+                q.pop();
+                for (int i = 0; i < word.size(); i++) {
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        string temp = word;
+                        temp[i] = ch;
+                        if (temp == endWord) {
+                            return levels+1;
+                        }
+                        if (dic.count(temp)) {
+                            q.push(temp);
+                            dic.erase(temp);
+                        }
                     }
-                }
+                };
             }
+            levels++;
         }
+
         return 0;
     }
 };
