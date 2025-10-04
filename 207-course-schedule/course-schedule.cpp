ff0b0100fb66418fc92e649ca4682f45e218897d
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-04 19:50:31 +0500

    Time: 11 ms (21.84%) | Memory: 20.2 MB (31.16%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int>& state, vector<vector<int>>& adj) {
-        // if the current node is already in state of visiting then cycle
-        // detected and we cannot finish courses so return false
-        if (state[node] == 1)
+    bool dfs(int node, vector<int>&state, vector<vector<int>>& adj){
+        if(state[node] == 1){ // check if already processing then we got a cycle 
             return false;
-        if (state[node] == 2)
-            return true; // we need to skip this one.
-        state[node] = 1;
-        for (auto& nei : adj[node]) {
-            if (!dfs(nei, state, adj))
+        }
+        if(state[node] == 2){ // check if its processed
+            return true;
+        }
+        state[node] = 1; // put into visitng 
+        for(auto &nei: adj[node]){
+           if(!dfs(nei, state, adj)){
                 return false;
+            }
         }
         state[node] = 2;
         return true;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // DAG, indegree+dfs(recursive stack);
+        // create adj list
         vector<vector<int>> adj(numCourses);
-        for (auto& vec : prerequisites) {
-            auto a = vec[0];
-            auto b = vec[1];
+        for(auto vec: prerequisites){
+            int a = vec[0]; int b = vec[1];
             adj[b].push_back(a);
         }
-        vector<int> state(
-            numCourses); // 0 unvi, 1 mean visitiin, 2 done visited.
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) {
-                if (!dfs(i, state, adj))
-                    return false; // cycle detected;
+        vector<int> state(numCourses, 0); // 0 univisted, 1 visiting, 2 is visited;
+        for(int i=0; i <numCourses; i++){
+            if(state[i] == 0){
+                if(!dfs(i, state, adj)){
+                    return false;
+                }
             }
         }
         return true;
