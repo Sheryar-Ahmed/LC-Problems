6090cfc18ac8f944ae48712e26768c9e773f1542
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-07 00:31:30 +0500

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.59%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -17,9 +17,8 @@ public:
             return true;
         if (!p || !q)
             return false;
-        if (p->val != q->val) {
+        if (p->val != q->val)
             return false;
-        }
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
