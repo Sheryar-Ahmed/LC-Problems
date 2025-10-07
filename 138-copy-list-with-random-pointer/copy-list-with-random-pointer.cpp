ca5ce3b0ea995253a43155e6331c52d484b1740d
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-07 23:24:45 +0500

    Time: 7 ms (52.54%) | Memory: 15.2 MB (46.41%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,7 +17,8 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head) return head;
+        if (!head)
+            return head;
         unordered_map<Node*, Node*> old_to_new;
         Node* curr = head;
         old_to_new[curr] = new Node(curr->val);
@@ -29,10 +30,6 @@ public:
         curr = head;
         while (curr) {
             old_to_new[curr]->next = old_to_new[curr->next];
-            curr = curr->next;
-        }
-        curr = head;
-        while (curr) {
             old_to_new[curr]->random = old_to_new[curr->random];
             curr = curr->next;
         }
