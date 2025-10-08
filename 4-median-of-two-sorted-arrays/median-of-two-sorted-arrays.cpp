6c644533182b2ffdfd1dcc932dd2f04f833f8ac4
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-08 17:38:36 +0500

    Time: 0 ms (100.00%) | Memory: 96.1 MB (10.62%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> merged;
-        
-        // Merge the two arrays
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
         while (i < nums1.size() && j < nums2.size()) {
             if (nums1[i] < nums2[j]) {
-                merged.push_back(nums1[i]);
-                i++;
+                merged.push_back(nums1[i++]);
             } else {
-                merged.push_back(nums2[j]);
-                j++;
+                merged.push_back(nums2[j++]);
             }
-        }
+        };
         while (i < nums1.size()) {
-            merged.push_back(nums1[i]);
-            i++;
+            merged.push_back(nums1[i++]);
         }
         while (j < nums2.size()) {
-            merged.push_back(nums2[j]);
-            j++;
+            merged.push_back(nums2[j++]);
         }
-
-        // Calculate the median
         int n = merged.size();
-        if (n % 2 == 1) {
-            return static_cast<double>(merged[n / 2]);
-        } else {
-            return (static_cast<double>(merged[n / 2 - 1]) + merged[n / 2]) / 2.0;
+        if(n % 2 != 0){
+            return static_cast<double>(merged[n/2]);
+        }else{
+            return static_cast<double>(merged[n/2-1]+merged[n/2]) / 2.0;
         }
     }
 };
