b55abed1953ac5b1823befd5ddd25c3c67b10fe4
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-13 10:23:49 +0500

    Time: 14 ms (18.52%) | Memory: 10.8 MB (70.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // same elements ki longest substring konsi ha after perfomin k
-        // operations on that substring
-        vector<int> freq(26, 0); // 0--> freq
+        unordered_map<char, int> window;
+        int mostFreq = 0;
+        int maxFreq = 0;
         int left = 0;
-        int res = 0;
-        int maxFreqCurrWindow = 0;
-        for (int right = 0; right < s.size(); right++) {
-            // frequency calculation
-            freq[s[right]-'A']++; // 'A'-'A' = 0
-            // expand
-            maxFreqCurrWindow = max(maxFreqCurrWindow, freq[s[right]-'A']);
-            // while invalid:
-            if(right-left+1 - maxFreqCurrWindow > k) {
-                freq[s[left]-'A']--;
+        int right = 0;
+        while (right < s.size()) {
+            window[s[right]]++;
+            mostFreq = max(mostFreq, window[s[right]]);
+            while ((right - left + 1) - mostFreq > k) {
+                window[s[left]]--;
                 left++;
-            }                
-            //     shrink from left
-            res = max(res, right-left+1);
-            // update result
+            }
+            maxFreq = max(maxFreq, right - left + 1);
+            right++;
         }
-        return res;
+        return maxFreq;
     }
 };
