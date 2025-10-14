eb4b40aeaaabb4fdb85e2fdf813197512e28fed2
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-14 10:21:12 +0500

    Time: 5 ms (68.52%) | Memory: 11.7 MB (25.32%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,42 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (t.empty() || s.empty()) return "";
+        unordered_map<char, int> tFreq;
+        for (auto& c : t) {
+            tFreq[c]++;
+        }
+        int left = 0;
+        int right = 0;
+        unordered_map<char, int> curr;
+        int have = 0;
+        int need = tFreq.size();
+        int minLen = INT_MAX;
+        int minStart = 0;
 
-        unordered_map<char, int> freq;
-        for (char c : t) freq[c]++;
-
-        unordered_map<char, int> window;
-        int have = 0, need = freq.size();
-        int left = 0, minLen = INT_MAX, minStart = 0;
-
-        for (int right = 0; right < s.size(); right++) {
-            char c = s[right];
-            window[c]++;
-
-            if (freq.count(c) && window[c] == freq[c]) {
+        while (right < s.size()) {
+            int c = s[right];
+            curr[c]++;
+            if (tFreq.count(c) && curr[c] == tFreq[c]) {
                 have++;
             }
-
-            // Shrink while valid
+            // shrink
             while (have == need) {
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
                     minStart = left;
                 }
-
-                // shrink from left
-                window[s[left]]--;
-                if (freq.count(s[left]) && window[s[left]] < freq[s[left]]) {
+                curr[s[left]]--;
+                if (tFreq.count(s[left]) && curr[s[left]] < tFreq[s[left]]) {
                     have--;
                 }
                 left++;
             }
+            right++;
         }
-
         return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
     }
 };
