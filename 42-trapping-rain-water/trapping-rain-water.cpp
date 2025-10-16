7303d180eb91e2ca7fe02e51e349a0d71eab6038
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-17 01:49:22 +0500

    Time: 0 ms (100.00%) | Memory: 27.2 MB (30.97%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int leftMax = 0;
-        int rightMax = 0;
-        int left = 0;
-        int right = n-1;
+        vector<int> maxLeft(n, 0);
+        vector<int> maxRight(n, 0);
+        int maxL = height[0];
+        for (int i = 1; i < n; i++) {
+            maxLeft[i] = maxL;
+            maxL = max(maxL, height[i]);
+        }
+        int maxR = height[n-1];
+        for (int i = n-1; i >= 0; i--) {
+            maxRight[i] = maxR;
+            maxR = max(maxR, height[i]);
+        }
         int water = 0;
-        while(left < right){
-            // if the left boundary is smaller and right is greater water will be stored according to the left boundary
-            if(height[left] < height[right]){
-                // if the max of left is greater than current left
-                if(height[left] > leftMax){
-                    leftMax = height[left];
-                }else{
-                    // calculate and add in the rain water
-                    water += leftMax-height[left];
-                }
-                left++;
-            }else{
-                // what if the right boundary is smaller then the left
-                if(height[right] >= rightMax){
-                    rightMax = height[right];
-                }else{
-                    water += rightMax-height[right];
-                }
-                right--;
+        for(int i=0; i < n; i++){
+            int contain = min(maxLeft[i], maxRight[i]) - height[i];
+            if(contain >= 0){
+                water+=contain;
             }
         }
         return water;
