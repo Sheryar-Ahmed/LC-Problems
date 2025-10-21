9db22e1c7153c1688a940db72b48c0f0fb31d7d6
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-21 23:39:41 +0500

    Time: 6 ms (29.66%) | Memory: 25.9 MB (7.44%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<string> hash;
+        unordered_set<string> m;
         int rows = board.size();
         int cols = board[0].size();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (board[i][j] != '.') {
-                    char n = board[i][j] - '0';
-                    string rowCheck =
-                        to_string(i) + " row check in " + to_string(n);
-                    string colCheck =
-                        to_string(j) + "col check in" + to_string(n);
-                    string cubeCheck = to_string(i / 3) + to_string(j / 3) +
-                                       "cube check in" + to_string(n);
-                    // row, col and cube checks;
-                    if (hash.find(rowCheck) != hash.end()) {
+                    string row = "row" + to_string(i) + to_string(board[i][j]);
+                    string col = "col" + to_string(j) + to_string(board[i][j]);
+                    string box = "box" +
+                                 to_string(i / 3) + to_string(j / 3) +
+                                 to_string(board[i][j]);
+                    if (m.count(row) || m.count(col) || m.count(box)) {
                         return false;
+                    } else {
+                        m.insert(row);
+                        m.insert(col);
+                        m.insert(box);
                     }
-                    if (hash.find(colCheck) != hash.end()) {
-                        return false;
-                    }
-                    if (hash.find(cubeCheck) != hash.end()) {
-                        return false;
-                    }
-                    hash.insert(rowCheck);
-                    hash.insert(colCheck);
-                    hash.insert(cubeCheck);
                 }
             }
         }
