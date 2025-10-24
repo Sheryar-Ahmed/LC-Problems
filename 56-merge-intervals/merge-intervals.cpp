a2cba2414de053ff6d605294ef6b7c13332950d6
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-24 10:40:02 +0500

    Time: 2 ms (93.17%) | Memory: 23.9 MB (58.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // sort it 
-        sort(intervals.begin(), intervals.end(), [](const auto&a, const auto&b){
-            return a[0] < b[0]; // ascending order
+        // sort the intervals by start
+        sort(intervals.begin(), intervals.end(), [](auto const &a, auto const &b){
+            return a[0] < b[0]; // ascending order by start
         });
-        // initially we put the intervals index 0 to the answer and check the last element
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
         for(int i=1; i < intervals.size(); i++){
-            int last = ans.back()[1];
-            int a = intervals[i][0];
-            int b = intervals[i][1];
-
-            if(last >= a){
-                // merge
-                ans.back()[1] = max(last, b);
-            }else{
-                ans.push_back(intervals[i]);
+            int s = intervals[i][0]; // 8, 1
+            int e = intervals[i][1]; // 10, 6
+            if(s <= ans.back()[1] && e > ans.back()[1]){
+                ans.back()[1] = e;
+            }else if(s > ans.back()[1] && e > ans.back()[1]){
+                ans.push_back({s, e});
             }
         }
         return ans;
