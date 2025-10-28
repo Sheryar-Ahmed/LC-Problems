de75ca632170000bd85c6ce8ab9544d3edafd59c
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-10-28 22:05:49 +0500

    Time: 176 ms (5.42%) | Memory: 22.9 MB (7.24%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -3,15 +3,17 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
         vector<bool> dp(n, false);
-        dp[0] = true; // for an empty string the sub string is always exists
+        dp[0] = true;
         unordered_set<string> dic(wordDict.begin(), wordDict.end());
-        for(int i=1; i <= n; i++){
+        for(int i=1; i <= n; i++){ // substr exclude the last one that is upto n
             for(int j=0; j < i; j++){
+                cout<<s.substr(j, i-j)<<endl;
                 if(dp[j] && dic.count(s.substr(j, i-j))){
                     dp[i] = true;
                     break;
                 }
             }
+            cout<<endl<<endl;
         }
         return dp[n];
     }
