31d30dc43a669328485ee79f35992058ace46590
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-02 03:44:06 +0500

    Time: 18 ms (84.19%) | Memory: 18 MB (71.50%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<int> dp(amount + 1, INT_MAX);
+        vector<int> dp(amount+1, INT_MAX);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (auto& c : coins) {
-                if (i - c >= 0 && dp[i - c] != INT_MAX) {
-                    dp[i] = min(dp[i], dp[i - c] + 1);
+        for(int i=1; i <= amount; i++){
+            // let' start with coins if we are able to make the amount
+            for(auto c: coins){
+                if(i-c >=0 &&  dp[i-c] != INT_MAX){
+                    dp[i] = min(dp[i], dp[i-c]+1);
                 }
             }
         }
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        return dp[amount] != INT_MAX ? dp[amount] : -1;
     }
 };
