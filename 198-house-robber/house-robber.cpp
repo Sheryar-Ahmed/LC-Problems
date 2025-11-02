18a5e32b48080b3efd7dc41b749e758088cf90a7
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-03 00:54:19 +0500

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.47%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,17 @@
 class Solution {
-vector<int> dp;
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        dp.assign(n, -1);
-        return solve(0, nums);
+        int n= nums.size();
+        vector<int> dp(n, -1);
+        return helper(nums, 0, dp);
     }
-    int solve(int n, vector<int>& nums){
-        if(n >= nums.size()) return 0;
-        if(dp[n] != -1) return dp[n];
-        // rob current house
-        int robCurr = nums[n]+solve(n+2, nums); // we can rob the next one.
-        int skipCurr = solve(n+1, nums); // immediatly rob the nextone;
-        return dp[n] = max(robCurr, skipCurr);
+    int helper(vector<int>& nums, int i, vector<int>& dp){
+        if(i >= nums.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+        // skip current
+        int skip = helper(nums, i+1, dp);
+        // rob 
+        int rob = nums[i]+helper(nums, i+2, dp);
+        return dp[i] = max(skip, rob);
     }
 };
