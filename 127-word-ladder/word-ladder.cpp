addd965bba5eed11961324eea737be83a11645e7
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-04 01:15:59 +0500

    Time: 67 ms (53.49%) | Memory: 19.5 MB (95.25%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -3,34 +3,32 @@ public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
         unordered_set<string> dic(wordList.begin(), wordList.end());
-        int levels = 1;
-        // bfs
-        if (!dic.count(endWord))
+        if (!dic.count(endWord)) {
             return 0;
+        }
         queue<string> q;
         q.push(beginWord);
+        int levels = 1;
         while (!q.empty()) {
             int size = q.size();
             while (size-- && !q.empty()) {
-                string word = q.front();
-                q.pop();
+                string word = q.front(); q.pop();
                 for (int i = 0; i < word.size(); i++) {
-                    for (char ch = 'a'; ch <= 'z'; ch++) {
-                        string temp = word;
-                        temp[i] = ch;
-                        if (temp == endWord) {
+                    string temp = word;
+                    for(char s='a'; s <= 'z'; s++){
+                        temp[i] = s;
+                        if(temp == endWord){
                             return levels+1;
                         }
-                        if (dic.count(temp)) {
+                        if(dic.count(temp)){
                             q.push(temp);
                             dic.erase(temp);
                         }
                     }
-                };
+                }
             }
             levels++;
         }
-
         return 0;
     }
 };
