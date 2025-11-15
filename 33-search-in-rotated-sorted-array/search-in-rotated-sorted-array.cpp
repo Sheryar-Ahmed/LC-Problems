28419eaabdcf85191739de4999926a877b2a4fd7
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-15 06:31:09 +0500

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // we need to find the lowest starting point.
-        // either the left half is sorted or right half is sorted based on that
-        // icrement or decrement it
         int left = 0;
         int right = nums.size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if(nums[mid] == target){
                 return mid;
             }
-            if (nums[left] <= nums[mid]) {
-                // left half is sorted;
+            // left part is sorted
+            else if (nums[left] <= nums[mid]) {
+                // target between then we need to shrink the right
                 if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
+                    right = mid-1;
                 } else {
-                    left = mid + 1;
+                    left = mid+1;
                 }
-            } else {
-                // rigth half is sorted;
-                if (nums[right] >= target && target > nums[mid]) {
-                    left = mid + 1;
+            } else { // right part is sorted
+                // check b/w the mid and right
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid+1;
                 } else {
-                    right = mid - 1;
+                    right = mid-1;
                 }
             }
         }
