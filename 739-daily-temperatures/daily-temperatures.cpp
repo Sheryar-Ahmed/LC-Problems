6297dd28e82ed32b7e378d1f0ab27a048ed72f3c
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-19 23:54:35 +0500

    Time: 35 ms (15.67%) | Memory: 107.3 MB (41.28%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> mst;
         int n = temperatures.size();
         vector<int> ans(n, 0);
+        stack<int> st; // stores indices, keeps a decreasing stack of temperatures
+
         for (int i = 0; i < n; i++) {
-            while (!mst.empty() && temperatures[i] > temperatures[mst.top()]) {
-                int idx = mst.top();
-                mst.pop();
-                ans[idx] = i-idx;
+            // whenever current temperature is greater
+            // it's the next greater temperature for the top of the stack
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int idx = st.top();
+                st.pop();
+                ans[idx] = i - idx; // number of days to wait
             }
-            mst.push(i);
+            st.push(i);
         }
+
         return ans;
     }
 };
