9f5b74c543e6cd6b88e753eb304e97305db0c68b
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-23 01:27:38 +0500

    Time: 12 ms (43.86%) | Memory: 14.2 MB (35.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size() < 1) return 0;
-        // maintain a window of set
         unordered_set<char> window;
-        int left = 0;
-        int right = 0;
-        int len = 0;
-        while(right < s.size()){
-            while(right < s.size() && window.count(s[right])){
+        int left = 0, right = 0;
+        int maxL = 0;
+
+        while (right < s.size()) {
+            // If duplicate found, shrink from left
+            while (window.count(s[right])) {
                 window.erase(s[left]);
                 left++;
             }
-            window.insert(s[right]); // expand it // abc
-            len = max(len, right-left+1);
-            right++; // 3
+
+            // Now safe to insert
+            window.insert(s[right]);
+            maxL = max(maxL, right - left + 1);
+            right++;
         }
-        return len;
+
+        return maxL;
     }
 };
