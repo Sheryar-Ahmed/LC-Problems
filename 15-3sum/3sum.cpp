5b04f71f2802dee20f9a8a44cf9e81f24781f540
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-11-29 19:36:34 +0500

    Time: 47 ms (68.10%) | Memory: 29 MB (89.78%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
         vector<vector<int>> ans;
-        for (int i = 0; i < n; i++) {
+        sort(nums.begin(), nums.end());
+        // fix 1 and find the two with two pinter technique
+        for (int i = 0; i < nums.size(); i++) {
             if (i > 0 && nums[i] == nums[i - 1])
-                continue; // skip duplicate.
+                continue;
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.size() - 1;
             while (left < right) {
-                int currSum = nums[i] + nums[left] + nums[right];
-                if (currSum == target) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
-                    left++;
+                if (nums[left] + nums[right] + nums[i] > 0) {
                     right--;
-                }
-                else if (currSum < target) {
+                } else if (nums[left] + nums[right] + nums[i] < 0) {
                     left++;
-                } else {
+                } else if (nums[left] + nums[right] + nums[i] == 0) {
+                    ans.push_back({nums[left], nums[right], nums[i]});
+                    // also check the next is a duplicate or not both in left
+                    // and right and skip
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++;
                     right--;
                 }
             }
