55255d21b025e41c7935067935322b1c550b551c
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-12-10 22:33:18 +0500

    Time: 11 ms (39.92%) | Memory: 36.5 MB (43.46%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int left = 0;
-        int right = arr.size() - k;
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    int n = arr.size();
+    
+    // Find the first index where arr[i] >= x
+    int pos = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+    
+    int left = pos - 1;
+    int right = pos;
 
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (x - arr[mid] > arr[mid + k] - x) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+    // Pick k closest numbers
+    vector<int> result;
+    while (k--) {
+        if (left < 0) {                 // must take from right
+            result.push_back(arr[right++]);
+        } else if (right >= n) {        // must take from left
+            result.push_back(arr[left--]);
+        } else {
+            // Compare distances to x
+            if (x - arr[left] <= arr[right] - x)
+                result.push_back(arr[left--]);
+            else
+                result.push_back(arr[right++]);
         }
-
-        return vector<int>(arr.begin() + left, arr.begin() + left + k);
     }
+
+    // We collected them in unsorted order, so sort
+    sort(result.begin(), result.end());
+    return result;
+}
+
 };
