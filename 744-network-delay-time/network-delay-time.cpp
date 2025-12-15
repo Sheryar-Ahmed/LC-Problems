c29f14485a13b429a9c0ce1c9ef6b7254db66fae
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-12-16 00:17:54 +0500

    Time: 71 ms (94.73%) | Memory: 44.2 MB (81.38%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<pair<int,int>>> adj; // u-->(v, w)
-        // build graphs
-        for(auto x: times){
-            adj[x[0]].emplace_back(x[1], x[2]);
+        vector<vector<pair<int,int>>> adj(n + 1);
+        for (auto &t : times) {
+            adj[t[0]].push_back({t[1], t[2]});
         }
 
-        // Dijksra setup
-        vector<int> dist(n+1, INT_MAX); // intially the distance of ecah node is infinite
-        dist[k] = 0; // start have zero distance
-        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<>> pq;
-        pq.emplace(0, k); // (dist, node)
-        while(!pq.empty()){
-            auto [currentDist, node] = pq.top(); pq.pop();
-            if(currentDist > dist[node]) continue; // dist[node] may have a large distance
+        vector<int> dist(n + 1, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
 
-            for(auto &[nei, w]: adj[node]){
-                if(dist[nei] > dist[node]+w){
-                    // if the nei has greater then we need to reduce it to small
-                    dist[nei] = dist[node]+w;
-                    pq.emplace(dist[nei], nei);
+        dist[k] = 0;
+        pq.push({0, k});
+
+        while (!pq.empty()) {
+            auto [d, node] = pq.top();
+            pq.pop();
+
+            if (d > dist[node]) continue;
+
+            for (auto &[nei, w] : adj[node]) {
+                if (dist[node] + w < dist[nei]) {
+                    dist[nei] = dist[node] + w;
+                    pq.push({dist[nei], nei});
                 }
             }
-
         }
-        int maxTime = *max_element(dist.begin()+1, dist.end());
-        return maxTime == INT_MAX ? -1 : maxTime;
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == INT_MAX) return -1;
+            ans = max(ans, dist[i]);
+        }
+
+        return ans;
     }
 };
