fba7982c0e6ac3f3bcfe67b537b6536485d28def
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-12-27 01:29:38 +0500

    Time: 0 ms (100.00%) | Memory: 97.4 MB (59.85%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() < 2) return 0;
         int maxP = 0;
-        int left = 0;
-        int right = 1;
-        while(right < prices.size()){
-            if(prices[left] < prices[right]){
-                maxP = max(maxP, prices[right]-prices[left]);
-            }else{
-                left = right;
-            }
-            right++;
+        int minBuy = prices[0];
+        for(auto &curr: prices){
+            maxP = max(maxP, curr-minBuy);
+            minBuy = min(minBuy, curr);
         }
+
         return maxP;
     }
 };
