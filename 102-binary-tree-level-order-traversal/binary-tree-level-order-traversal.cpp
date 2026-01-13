8b7aad5428f50dcee1b5a6f771b012c39af3923d
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-01-14 01:04:58 +0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (16.18%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,28 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if(!root) return {};
+        if (!root)
+            return {};
         queue<TreeNode*> q;
         q.push(root);
         vector<vector<int>> ans;
-        while(!q.empty()){
-            int size = q.size();
+        while (!q.empty()) {
             vector<int> curr;
-            while(size--){
-                TreeNode* front = q.front(); q.pop();
-                curr.push_back(front->val);
-                if(front->left){
-                    q.push(front->left);
-                }
-                if(front->right){
-                    q.push(front->right);
-                }
+            int size = q.size();
+            while (size-- && !q.empty()) {
+                auto fr = q.front();
+                q.pop();
+                curr.push_back(fr->val);
+                if (fr->left)
+                    q.push(fr->left);
+
+                if (fr->right)
+                    q.push(fr->right);
             }
             ans.push_back(curr);
         }
