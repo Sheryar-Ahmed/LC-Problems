26a2e0ececdd88e032d1f15e92dd4eb2fa23fa10
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-01-28 00:44:39 +0500

    Time: 4 ms (5.30%) | Memory: 17.8 MB (29.64%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> memo(n, -1);
-        return min(solve(n-1, memo, cost), solve(n-2, memo, cost));
+    vector<int> dp;
+
+    int dfs(int i, vector<int>& cost) {
+        if (i >= cost.size())
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+
+        return dp[i] = cost[i] + min(dfs(i + 1, cost), dfs(i + 2, cost));
     }
-    int solve(int n, vector<int>& memo, vector<int>& cost){
-        if(memo[n] != -1) return memo[n];
-        if(n == 0 || n == 1) return cost[n];
-        return memo[n] = cost[n] + min(solve(n-1, memo, cost), solve(n-2, memo, cost));
+    int minCostClimbingStairs(vector<int>& cost) {
+        // we have two choices initially either from index 0 or 1
+        // then we have choices at each step either i+1, or i+2; we can take the
+        // minimum at that position and store that in our dp[i] and our base
+        // condition would be i > cost.size(); then we will return moreover this
+        // is sufficient just do the recursion first will add dp letter at start
+        // we can also have min(dfs(0), dfs(1)); and then in each dfs we have
+        // i+1, i+2;
+        dp.assign(cost.size(), -1);
+        return min(dfs(0, cost), dfs(1, cost));
     }
 };
