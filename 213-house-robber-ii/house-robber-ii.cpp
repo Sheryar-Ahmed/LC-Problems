f0cc7979493e97d2d7697bd1642f33c1d79d198f
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-01-31 00:58:39 +0500

    Time: 0 ms (100.00%) | Memory: 10.7 MB (53.45%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int robFrom(int i, int end, vector<int>& nums, vector<int>& memo) {
-        if (i > end) return 0;
-        if (memo[i] != -1) return memo[i];
-
-        return memo[i] = max(nums[i] + robFrom(i + 2, end, nums, memo),
-                             robFrom(i + 1, end, nums, memo));
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
+        if(n == 0) return 0;
+        if(n < 2) return nums[0];
+        vector<int> memo1(n, -1);
+        vector<int> memo2(n, -1);
 
-        vector<int> memo1(n, -1);  // rob from 0 to n-2
-        vector<int> memo2(n, -1);  // rob from 1 to n-1
-
-        int rob1 = robFrom(0, n - 2, nums, memo1);  // exclude last
-        int rob2 = robFrom(1, n - 1, nums, memo2);  // exclude first
+        int rob1 = helper(n - 2, nums, 0, memo1);
+        int rob2 = helper(n - 1, nums, 1, memo2);
 
         return max(rob1, rob2);
+    }
+    int helper(int end, vector<int>& nums, int i, vector<int>& dp) {
+        if (i > end)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        // skip current
+        int skip = helper(end, nums, i + 1, dp);
+        // rob
+        int rob = nums[i] + helper(end, nums, i + 2, dp);
+        return dp[i] = max(skip, rob);
     }
 };
