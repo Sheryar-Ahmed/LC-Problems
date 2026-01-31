2b785d975aec1c6219aea822081399a70dfb2cc8
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-01-31 19:33:54 +0500

    Time: 1023 ms (5.52%) | Memory: 13.6 MB (49.34%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int start = 0;
-        int end = 1;
-        for (int i = 0; i < s.size() - 1; i++) {
-            centerSolve(s, i, i, start, end);
-            centerSolve(s, i, i+1, start, end);
+        string res = "";
+        int resLen = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = i; j < s.size(); j++) {
+                int l = i, r = j;
+                while (l < r && s[l] == s[r]) {
+                    l++;
+                    r--;
+                }
+
+                if (l >= r && resLen < (j - i + 1)) {
+                    res = s.substr(i, j - i + 1);
+                    resLen = j - i + 1;
+                }
+            }
         }
-        return s.substr(start, end);
-    }
-    void centerSolve(string& s, int i, int j, int &start, int &end) {
-        // now check if the current character expands and make the string
-        // palindrome
-        while (i >= 0 && j <= s.size() - 1 && s[i] == s[j]) {
-            i--;
-            j++;
-        }
-        // now compute string size j-i+1;
-        if (j - i - 1 > end) {
-            start = i + 1;
-            end = j - i - 1;
-        }
+
+        return res;
     }
 };
