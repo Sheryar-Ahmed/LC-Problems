c21933d0261fb1624bc539e252f9fdd83c197287
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-02-19 02:09:20 +0500

    Time: 3 ms (19.90%) | Memory: 74.6 MB (15.11%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 0); // -2, -1, -1, 3, 3, 5, 6, 6
+        vector<int> dp(n);
         dp[0] = nums[0];
-        int ans = nums[0]; // 6
-        for (int i = 1; i < nums.size(); i++) {
-            dp[i] = max(nums[i], dp[i-1] + nums[i]);
+        int ans = nums[0];
+        for(int i=1; i < n; i++){
+            dp[i] = max(nums[i], dp[i-1]+nums[i]);
             ans = max(ans, dp[i]);
         }
-
-        return ans;
+        return ans; 
     }
 };
