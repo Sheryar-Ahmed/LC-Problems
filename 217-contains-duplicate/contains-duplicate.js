69ae708437879e9b1e2cf25e7ef558e04160e6cc
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-04-13 22:46:58 +0500

    Time: 65 ms (11.07%) | Memory: 66.6 MB (94.47%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -4,12 +4,19 @@
  */
 var containsDuplicate = function(nums) {
     if(nums.length < 2) return false;
-    let set = new Set();
-    for(x of nums){
-        if(set.has(x)){
-            return true;
-        }
-        set.add(x);
+    // with sort
+    nums.sort((a,b) => a-b);
+    for(let i=0; i < nums.length; i++){
+        if(i > 0 && nums[i] == nums[i-1]) return true;
     }
     return false;
+    // with set
+    // let set = new Set();
+    // for(x of nums){
+    //     if(set.has(x)){
+    //         return true;
+    //     }
+    //     set.add(x);
+    // }
+    // return false;
 };
