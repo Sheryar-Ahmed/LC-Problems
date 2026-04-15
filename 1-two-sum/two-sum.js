483b80d1770554e95683623d06e8e5afd8698036
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-04-16 01:39:47 +0500

    Time: 2 ms (66.50%) | Memory: 55 MB (50.70%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,21 +4,32 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // let's just use the two pointer technique with sort
-    let arr = nums.map((val, ind) => [val, ind]);
-    console.log(arr);
-    arr.sort((a,b) => a[0]-b[0]);
-    let i=0;
-    let j=nums.length-1;
-    while(i <  j){
-        let sum = arr[i][0]+arr[j][0];
-        if(sum == target){
-            return [arr[i][1], arr[j][1]];
-        }else if(sum < target){
-            i++;
-        } else {
-            j--;
+    // let's just solve this using hashmap
+    let map = new Map();
+    for(let i=0; i < nums.length; i++){
+        let complement = target - nums[i];
+        if(map.has(complement)){
+            return [i, map.get(complement)];
         }
-    }    
-    return [];
+        map.set(nums[i], i);
+    }
+
+
+    // let's just use the two pointer technique with sort
+    // let arr = nums.map((val, ind) => [val, ind]);
+    // console.log(arr);
+    // arr.sort((a,b) => a[0]-b[0]);
+    // let i=0;
+    // let j=nums.length-1;
+    // while(i <  j){
+    //     let sum = arr[i][0]+arr[j][0];
+    //     if(sum == target){
+    //         return [arr[i][1], arr[j][1]];
+    //     }else if(sum < target){
+    //         i++;
+    //     } else {
+    //         j--;
+    //     }
+    // }    
+    // return [];
 };
