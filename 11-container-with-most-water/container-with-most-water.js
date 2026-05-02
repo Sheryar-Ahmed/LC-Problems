ef40510b2fb88349626b922a65c03a7cb9e2d870
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2026-05-03 03:57:21 +0500

    Time: 2 ms (76.17%) | Memory: 62.7 MB (72.48%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,27 +3,37 @@
  * @return {number}
  */
 var maxArea = function (height) {
-    //  find the max number and then try to find the second maximum and so on and compare the difference we can get our results its O(n) solution.
+    //  find the max number and then try to find the second maximum and so on and compare the difference we can get our results its O(n logn) solution.
     // 2nd solution, two pointer why because of the search space we need to search something
     // move to smaller one untill L < R
-    let indexed = height.map((h, i) => [h, i]);
+    // let indexed = height.map((h, i) => [h, i]);
 
-    // sort descending by height
-    indexed.sort((a, b) => b[0] - a[0]);
+    // // sort descending by height
+    // indexed.sort((a, b) => b[0] - a[0]);
 
-    let max = 0;
-    let minIndex = indexed[0][1];
-    let maxIndex = indexed[0][1];
+    // let max = 0;
+    // let minIndex = indexed[0][1];
+    // let maxIndex = indexed[0][1];
 
-    for (let i = 1; i < indexed.length; i++) {
-        let [h, idx] = indexed[i];
+    // for (let i = 1; i < indexed.length; i++) {
+    //     let [h, idx] = indexed[i];
 
-        let width = Math.max(Math.abs(idx - minIndex), Math.abs(idx - maxIndex));
-        max = Math.max(max, width * h);
+    //     let width = Math.max(Math.abs(idx - minIndex), Math.abs(idx - maxIndex));
+    //     max = Math.max(max, width * h);
 
-        minIndex = Math.min(minIndex, idx);
-        maxIndex = Math.max(maxIndex, idx);
+    //     minIndex = Math.min(minIndex, idx);
+    //     maxIndex = Math.max(maxIndex, idx);
+    // }
+
+    // return max;
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+    while (left < right) {
+        let w = right - left;
+        maxArea = Math.max(maxArea, w * (Math.min(height[left], height[right])));
+        if (height[left] <= height[right]) left++
+        else right--
     }
-
-    return max;
+    return maxArea;
 };
